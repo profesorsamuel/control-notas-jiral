e7f0159909388a81a05075861788c4b4d39b19b9
profesorsamuel <profesorsamuelortega@gmail.com>
2026-08-06 08:48:19 -0500

    Update Supabase client import to version 2

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,6 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
-
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 const supabaseUrl = "https://luewrpzgetqslxqmdcxv.supabase.co";
 const supabaseKey = "sb_publishable_zJc68aiTxJs7HBgaqeHxrQ_JDcm91xG";
-
 export const supabase = createClient(
   supabaseUrl,
   supabaseKey
